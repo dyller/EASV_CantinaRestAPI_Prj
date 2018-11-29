ac7982fceae353550f3fba48365839b30f1ae621
Sub-Z3r0 <andersen19@gmail.com>
2018-11-29 14:39:31 +0100

    push
    login works in postman

---
+++ EASV_Cantina/Controllers/AuthenticationController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CantinaApp.Core.DomainServices;
+using CantinaApp.Core.Entity.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EASV_CantinaRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IUserRepositories<Users> repository;
+
+        public AuthenticationController(IUserRepositories<Users> repos)
+        {
+            repository = repos;
+        }
+
+        // GET: api/Todo
+        [Authorize]
+        [HttpGet]
+        public IEnumerable<Users> GetAll()
+        {
+            return repository.GetAll();
+        }
+
+        // GET: api/Todo/5
+        [Authorize(Roles = "Administrator")]
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(long id)
+        {
+            var item = repository.Get(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        // POST: api/Todo
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
+        public IActionResult Post([FromBody] Users item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            repository.Add(item);
+
+            return CreatedAtRoute("Get", new { id = item.Id }, item);
+        }
+
+        // PUT: api/Todo/5
+        [Authorize(Roles = "Administrator")]
+        [HttpPut("{id}")]
+        public IActionResult Put(long id, [FromBody] Users item)
+        {
+            if (item == null || item.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = repository.Get(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            todo.Username = item.Username;
+            todo.IsAdmin = item.IsAdmin;
+
+            repository.Edit(todo);
+            return new NoContentResult();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [Authorize(Roles = "Administrator")]
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var todo = repository.Get(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            repository.Remove(id);
+            return new NoContentResult();
+        }
+    }
+}
---
+++ EASV_Cantina/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CantinaApp.Core.DomainServices;
+using CantinaApp.Core.Entity.Models;
+using EASV_CantinaRestAPI.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EASV_CantinaRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUserRepositories<Users> repository;
+        private IAuthenticationHelper authenticationHelper;
+
+        public UserController(IUserRepositories<Users> repos, IAuthenticationHelper authService)
+        {
+            repository = repos;
+            authenticationHelper = authService;
+        }
+
+
+        [HttpPost]
+        public IActionResult Login([FromBody]LoginInputModel model)
+        {
+            var user = repository.GetAll().FirstOrDefault(u => u.Username == model.Username);
+
+            // check if username exists
+            if (user == null)
+                return Unauthorized();
+
+            // check if password is correct
+            if (!authenticationHelper.VerifyPasswordHash(model.Password, user.PasswordHash, user.PasswordSalt))
+                return Unauthorized();
+
+            // Authentication successful
+            return Ok(new
+            {
+                username = user.Username,
+                token = authenticationHelper.GenerateToken(user)
+            });
+        }
+    }
+}
